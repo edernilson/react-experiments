bfdf7efff42eb95b31bd6bdba13f29923a1c526d
Eder Nilson <eder.nilson@gmail.com>
2026-03-15 20:58:05 -0300

    [react-skillsoft-app] Create static pages header, container, aside, and footer

--- react-skillsoft-app/src/App.test.js
+++ react-skillsoft-app/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders header content', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Skillsoft Weight Tracker/i);
   expect(linkElement).toBeInTheDocument();
 });
---
+++ react-skillsoft-app/src/components/container.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Main from "./main";
+import Aside from "./aside";
+
+function Container() {
+  return (
+    <div id="container">
+      <Main />
+      <Aside />
+    </div>
+  );
+}
+
+export default Container;
